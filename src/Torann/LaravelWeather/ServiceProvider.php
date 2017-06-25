4ab6871e0bc40ba18a837d5ac3f918c591231878
Marti Markov <marti.markov@icloud.com>
2017-06-25 14:20:27 +0100

    Laravel 5.4 compatibility

--- src/Torann/LaravelWeather/ServiceProvider.php
+++ src/Torann/LaravelWeather/ServiceProvider.php
@@ -4,47 +4,46 @@ use Illuminate\Foundation\AliasLoader;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		// Auto create app alias with boot method.
-		AliasLoader::getInstance()->alias('Weather', 'Torann\LaravelWeather\Facade');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Auto create app alias with boot method.
+        AliasLoader::getInstance()->alias('Weather', 'Torann\LaravelWeather\Facade');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['torann.weather'] = $this->app->singleton(function($app)
-		{
-      // Get config
-      $config = config('weather');
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('torann.weather', function ($app) {
+            // Get config
+            $config = config('weather');
 
-			return new Weather($app->cache, $app->view, $config);
-		});
-	}
+            return new Weather($app->cache, $app->view, $config);
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 }
